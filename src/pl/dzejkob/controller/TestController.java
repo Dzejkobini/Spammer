fd70bf24a40a9f2b5296c905f973b5cb35f3b644
Jacob <qba191@gmail.com>
2016-02-05 15:41:58 +0100

    Init

---
+++ src/pl/dzejkob/controller/TestController.java
@@ -0,0 +1,5 @@
+package pl.dzejkob.controller;
+
+public class TestController {
+
+}
---
+++ src/pl/dzejkob/manager/MailManager.java
@@ -0,0 +1,8 @@
+package pl.dzejkob.manager;
+
+public class MailManager {
+	public MailManager()
+	{
+		System.out.println("Test");
+	}
+}
